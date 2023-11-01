1ad17dc13525d46a30943334bc3e5f3649f4da0d
JandroThielman <9020498@student.zadkine.nl>
2023-11-01 09:31:48 +0100

    jandro

--- Javascript.js
+++ Javascript.js
@@ -1,32 +1,47 @@
-var d;
-var m;
-var sum;
-var d;
-
 function duplicate(){
     var d = Getal1 * Getal2;
     Getal1 = document.getElementById("Getal1").value;
     Getal2 = document.getElementById("Getal2").value;
-    document.getElementById("bereken").innerHTML = d;
+
+    if (d <= 0) {
+        document.getElementById("bereken").innerHTML = "Het getaal is te laag";
+    } else {
+        document.getElementById("bereken").innerHTML = d;
+    }
 }
 
 function min(){
     var m = Getal1 - Getal2;
     Getal1 = document.getElementById("Getal1").value;
     Getal2 = document.getElementById("Getal2").value;
-    document.getElementById("bereken").innerHTML = m;
+
+    if (m <= 0) {
+        document.getElementById("bereken").innerHTML = "Het getaal is te laag";
+    } else {
+        document.getElementById("bereken").innerHTML = m;
+    }
 }
 
 function plus(){
     var sum = Number(Getal1) + Number(Getal2);
     Getal1 = document.getElementById("Getal1").value;
     Getal2 = document.getElementById("Getal2").value;
-    document.getElementById("bereken").innerHTML = sum;
+
+    if (sum <= 0) {
+        document.getElementById("bereken").innerHTML = "Het getaal is te laag";
+    } else {
+        document.getElementById("bereken").innerHTML = sum;
+    }
 }
 
 function delen(){
-    var d = Getal1 / Getal2;
+    var x = Getal1 / Getal2;
     Getal1 = document.getElementById("Getal1").value;
     Getal2 = document.getElementById("Getal2").value;
-    document.getElementById("bereken").innerHTML = d;
+    
+    if (x <= 0) {
+        document.getElementById("bereken").innerHTML = "Het getaal is te laag";
+    } else {
+        document.getElementById("bereken").innerHTML = x;
+    }
 }
